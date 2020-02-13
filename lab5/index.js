2b13e109c7d286e6687518c15270566886e2400a
e-villa <evilla@users.noreply.github.com>
2020-02-13 16:14:18 -0500

    Lab 5 Completed

---
+++ lab5/index.js
@@ -0,0 +1,57 @@
+const Express = require("express");
+const App = Express();
+const port = 80;
+
+const chalk = require("chalk");
+const pokemon = require('json-pokemon');
+
+
+App.use("/", Express.static("public"));
+
+//localhost/id/#id
+App.get("/id/:id", function(req, res) {
+  let result = {"error": "Could not find that id! Sorry! :c"};
+
+        pokemon.forEach((value) => {
+            if(value.id == req.params.id) {
+                result = value;
+            }
+        });
+
+        if(result.error) {
+            // In red
+            console.log(chalk.red(req.path));
+        } else {
+            // In green
+            console.log(chalk.green(req.path));
+        }
+
+       res.send(result);
+});
+
+/*localhost/name/"nameofPokemon"
+which the name of the pokemon can be found either capitalized or lowercase
+*/
+App.get("/name/:name", function(req, res) {
+  let result = {"error": "Could not find that name! Sorry! :c"};
+
+        pokemon.forEach((value) => {
+            if(value.name == req.params.name || value.name.toLowerCase() == req.params.name ) {
+                result = value;
+            }
+        });
+
+        if(result.error) {
+            // In red
+            console.log(chalk.red(req.path));
+        } else {
+            // In green
+            console.log(chalk.green(req.path));
+        }
+
+         res.send(result);
+});
+
+App.listen(port, function() {
+    console.log("Server running!");
+});
